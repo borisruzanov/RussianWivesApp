9c3d0244c7f16400d19873affba1c89a59f60939
borisruzanov <boris.ruzanov@gmail.com>
2020-03-04 15:05:20 -0500

    Customviewpager bug fix / set orientation mode

--- app/src/main/java/com/borisruzanov/russianwives/mvp/ui/main/adapter/CustomViewPager.java
+++ app/src/main/java/com/borisruzanov/russianwives/mvp/ui/main/adapter/CustomViewPager.java
@@ -711,6 +711,7 @@ public class CustomViewPager extends ViewPager {
                 = ParcelableCompat.newCreator(new ParcelableCompatCreatorCallbacks<SavedState>() {
             @Override
             public SavedState createFromParcel(Parcel in, ClassLoader loader) {
+
                 return new SavedState(in, loader);
             }
 
